61f71c98ae1bc557333a740591d2a01744a3061f
vishesh vashista <vishesh.vashista@gmail.com>
2021-02-21 13:43:44 +0000

    Uploaded file for Task 4

--- upload_docker.sh
+++ upload_docker.sh
@@ -3,13 +3,13 @@
 
 # Assumes that an image is built via `run_docker.sh`
 
-# Step 1:
-# Create dockerpath
-# dockerpath=<your docker ID/path>
+# Step 1: Create dockerpath
+dockerpath="visheshvashista/udacitymicroservicesproject"
 
-# Step 2:  
-# Authenticate & tag
+# Step 2: Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username visheshvashista
+docker image tag app:latest $dockerpath
 
-# Step 3:
-# Push image to a docker repository
+# Step 3: Push image to a docker repository
+docker image push $dockerpath

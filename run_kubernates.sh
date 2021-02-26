49b547eaca3e31f958512535324595c1c821e3d3
visheshvashista <35123843+visheshvashista@users.noreply.github.com>
2021-02-27 02:29:53 +0530

    Update run_kubernates.sh

--- run_kubernates.sh
+++ run_kubernates.sh
@@ -2,17 +2,14 @@
 
 # This tags and uploads an image to Docker Hub
 
-# Step 1:
-# This is your Docker ID/path
-# dockerpath=<>
+# Step 1: # This is your Docker ID/path
+dockerpath="visheshvashista/udacitymicroservicesproject8"
 
-# Step 2
-# Run the Docker Hub container with kubernetes
+# Step 2: # Run the Docker Hub container with kubernetes
+kubectl run udacitymicroservices8 --image=$dockerpath --port=80
 
+# Step 3: # List kubernetes pods
+kubectl get pods
 
-# Step 3:
-# List kubernetes pods
-
-# Step 4:
-# Forward the container port to a host
-
+# Step 4: # Forward the container port to a host
+kubectl port-forward udacitymicroservices8 8080:80                                                                                                                                                                                                                                           ~                                                         
